b4a0745b22331f0bf2db58d695fcee07ffb78fea
JoaoUgulini <joao.2023001218@aluno.iffar.edu.br>
2025-11-27 11:25:02 -0300

    Correção de senha teste dnv

--- server-ugulini/src/routes/usuario.routes.js
+++ server-ugulini/src/routes/usuario.routes.js
@@ -5,22 +5,18 @@ const controller = require("../controllers/usuario.controller");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-// ROTA TEMPORÁRIA PARA RESETAR A SENHA (REMOVER DEPOIS)
 router.get("/resetarSenha", async (req, res) => {
   try {
-    // HASH DA SENHA NOVA → Contabil20
     const hash = "$2a$10$xysPA0cdf.NC11FL8coQTOFnPE.N65f.hvIjpzKfUFRq4YYeCob16";
 
     await prisma.usuario.update({
-      where: { cpf: "03032541069" },
+      where: { id: 1 }, 
       data: { senha: hash }
     });
 
     res.send("Senha resetada com sucesso!");
-    
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Erro ao resetar senha: " + error.message);
+  } catch (err) {
+    res.status(500).send("Erro ao resetar senha: " + err.message);
   }
 });
 
