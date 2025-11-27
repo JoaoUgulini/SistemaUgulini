d3ffdef94e02fb8b279d9b515fe88bc8c4636755
JoaoUgulini <joao.2023001218@aluno.iffar.edu.br>
2025-11-27 11:21:08 -0300

    Correção de senha teste

--- server-ugulini/src/routes/usuario.routes.js
+++ server-ugulini/src/routes/usuario.routes.js
@@ -1,15 +1,14 @@
-const controller = require("../controllers/usuario.controller");
-const auth = require("../middlewares/auth");
-module.exports = router;
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/usuario.controller");
+
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-// ROTA TEMPORÁRIA PARA RESETAR SENHA
+// ROTA TEMPORÁRIA PARA RESETAR A SENHA (REMOVER DEPOIS)
 router.get("/resetarSenha", async (req, res) => {
   try {
+    // HASH DA SENHA NOVA → Contabil20
     const hash = "$2a$10$xysPA0cdf.NC11FL8coQTOFnPE.N65f.hvIjpzKfUFRq4YYeCob16";
 
     await prisma.usuario.update({
@@ -18,12 +17,17 @@ router.get("/resetarSenha", async (req, res) => {
     });
 
     res.send("Senha resetada com sucesso!");
-  } catch (err) {
-    res.status(500).send("Erro: " + err.message);
+    
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Erro ao resetar senha: " + error.message);
   }
 });
 
-// ROTAS ORIGINAIS
+// ROTA DE LOGIN
 router.post("/login", controller.login);
+
+// OUTRAS ROTAS (se existirem)
+router.get("/", (req, res) => res.send("Rota usuário OK"));
 
 module.exports = router;
