4ac5dc0157ffb2018f738b08037e59c0e87018dd
João Ugulini <joao.2023001218@aluno.iffar.edu.br>
2025-11-28 08:55:53 -0300

    fix:pt2 cloudflare r2

--- server-ugulini/src/config/r2.js
+++ server-ugulini/src/config/r2.js
@@ -1,9 +1,18 @@
 const { S3Client } = require("@aws-sdk/client-s3");
+const { NodeHttpHandler } = require("@aws-sdk/node-http-handler");
+const https = require("https");
 
 const r2 = new S3Client({
   region: "auto",
+  endpoint: process.env.CF_R2_ENDPOINT,
   forcePathStyle: true,
-  endpoint: process.env.CF_R2_ENDPOINT, 
+  requestHandler: new NodeHttpHandler({
+    httpsAgent: new https.Agent({
+      keepAlive: true,
+      maxSockets: 50,
+      minVersion: "TLSv1.2",
+    })
+  }),
   credentials: {
     accessKeyId: process.env.CF_R2_ACCESS_KEY_ID,
     secretAccessKey: process.env.CF_R2_SECRET_ACCESS_KEY
