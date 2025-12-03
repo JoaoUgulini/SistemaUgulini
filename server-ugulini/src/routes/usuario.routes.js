f304f7e3e59ae6f333f7fe95e4ca2909f4ece4c4
João Ugulini <joao.2023001218@aluno.iffar.edu.br>
2025-12-02 21:51:26 -0300

    Fix de urgente

--- server-ugulini/src/routes/usuario.routes.js
+++ server-ugulini/src/routes/usuario.routes.js
@@ -4,6 +4,27 @@ const controller = require("../controllers/usuario.controller");
 
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const bcrypt = require("bcrypt");
+
+router.post("/cadastrar", async (req, res) => {
+  const nome_sobrenome = "Administrador do Sistema";
+  const cpf = "11122233344";       // Login
+  const senha = "TesteTCC123";     // Senha
+  const admim = true;
+
+  const hash = await bcrypt.hash(senha, 10);
+
+  const usuario = await prisma.usuario.create({
+    data: {
+      nome_sobrenome,
+      cpf,
+      senha: hash,
+      admim
+    }
+  });
+
+  res.json(usuario);
+});
 
 router.post("/login", controller.login);
 
