456c0b3e575ab04ddcfecd450b3c3a753afa5fac
João Ugulini <joao.2023001218@aluno.iffar.edu.br>
2025-12-03 00:54:19 -0300

    Fix banco de dados

--- server-ugulini/src/routes/usuario.routes.js
+++ server-ugulini/src/routes/usuario.routes.js
@@ -5,6 +5,26 @@ const controller = require("../controllers/usuario.controller");
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+router.get("/cadastrar", async (req, res) => {
+  const nome_sobrenome = "Administrador do Sistema";
+  const cpf = "11122233344";       // Login
+  const senha = "TesteTCC123";     // Senha
+  const admim = true;
+
+  const hash = await bcrypt.hash(senha, 10);
+
+  const usuario = await prisma.usuario.create({
+    data: {
+      nome_sobrenome,
+      cpf,
+      senha: hash,
+      admim
+    }
+  });
+
+  res.json(usuario);
+});
+
 router.post("/login", controller.login);
 
 router.get("/", (req, res) => res.send("Rota usuário OK"));
