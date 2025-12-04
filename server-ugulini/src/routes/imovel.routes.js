af223844f253584c022301aa8182d9e461fb21c4
JoaoUgulini <joao.2023001218@aluno.iffar.edu.br>
2025-12-04 08:36:08 -0300

    Fix: route pt2

--- server-ugulini/src/routes/imovel.routes.js
+++ server-ugulini/src/routes/imovel.routes.js
@@ -5,15 +5,16 @@ const router = Router();
 
 router.get("/cidades", controller.getCidades);
 router.get("/bairros", controller.getBairros);
+
 router.get("/filter", controller.filter);
 
 router.post("/", upload.array("fotos", 15), controller.create);
 router.put("/:id", upload.array("fotos", 15), controller.update);
+
 router.patch("/inativar/:id", controller.inativar);
 
-router.get("/", controller.list);
 router.get("/all", controller.listAll);
-
+router.get("/", controller.list);
 router.get("/:id", controller.getById);
 
 module.exports = router;
